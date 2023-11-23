d15dad7e68a403c965fe4b6ecafe2c91fc915242
Giogro <giogro2000@gmail.com>
2023-11-23 23:17:03 +0100

    working fetch request and starting to handle the data.

--- script.js
+++ script.js
@@ -2,22 +2,17 @@ var input = document.getElementById("search_bar") ;
 var target = document.getElementById("target") ;
 var button = document.getElementById("search_button") ;
 var s_par = document.getElementById("star_data") ;
-button.addEventListener("click", updateParagraph) ;
+button.addEventListener("click", function(){
+    retrieve_info(input.value) ;
+}) ;
 input.addEventListener("keydown", function(event){
     if(event.key=="Enter") {updateParagraph() ;}
-})
-fetchData() ;
+}) ;
 
 
 
-
-//replaces the target element with the input value
-function updateParagraph() {
-    target.textContent=input.value ;
-}
-
-//fetches names and IDs
-async function fetchData() {
+//main func
+function retrieve_info(name) {
     var query = `
       query AllFilms {
         allPeople {
@@ -29,19 +24,30 @@ async function fetchData() {
       }
     `;
 
-    try {
-        const response = await fetch('https://swapi-graphql.netlify.app/.netlify/functions/index', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-                'Accept': 'application/json'},
-    body: JSON.stringify({query: query})
-    }) ;
-    const data = await response.json() ;
-    console.log("DATA:", data) ;
-    return data ; 
-    } 
-    catch(error) {
-        console.log("ERROR:", error) ;
-    }
+    fetch('https://swapi-graphql.netlify.app/.netlify/functions/index', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json',
+        },
+        body: JSON.stringify({ query: query }),
+    })
+    .then(response => response.json()) //make data into JSON
+    .then(data => {
+        console.log('Data received:', data.data.allPeople.people); //for debugging
+        var namelist = data.data.allPeople.people ;
+        console.log(namelist) ;
+      for(var i=0; i<namelist.length ; i++) {
+            if(name==namelist[i].name) {
+                //showinfo(person.id) ;
+                console.log(namelist[i].name) ;
+                break ;
+         }
+        }
+    }) 
+    .catch(error => {
+        console.error('Error fetching data:', error);
+    });
 }
+
+//click-->retrieve list of names-->find name_id in list-->query info for id-->process and show info
